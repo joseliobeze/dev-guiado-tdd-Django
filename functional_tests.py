2311b39ef257367611cc21c767bd70b70f15d915
jbs <joseliobeze@gmail.com>
2019-04-29 16:39:33 -0300

    correcao tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-"""from selenium import webdriver
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.bowser.title)
+        self.fail('Finish the test!')
 
-assert 'Django' in browser.title"""
+        # She is invited to enter a to-do item straight away
+        #[...rest of comments as before]
+    
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
