819c04df1026ae2b9320ddd729573c4acd6eb4b1
jbs <joseliobeze@gmail.com>
2019-04-29 23:03:42 -0300

    testes passando no tempate home.hmtl

--- lists/tests.py
+++ lists/tests.py
@@ -15,4 +15,4 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
