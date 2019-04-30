29831a80c2768f8aa85ff30138231a852ca9fd97
jbs <joseliobeze@gmail.com>
2019-04-30 19:03:27 -0300

    Teste no template

--- functional_tests.py
+++ functional_tests.py
@@ -21,32 +21,27 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'), 
-            'Enter a to-do item'
-            )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('User peacock feathers to make a fly')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "new to-do item not appear in table"
-        )
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
-        # methodical)
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        self.assertIn('2: Buy peacock feathers to make a fly', [row.text for row in rows])
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
         self.fail('Finish the test!')
 
